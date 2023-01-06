79d64b0a35e7445ba5267d5263d69db76f68bd71
lyes kherbiche <kerbiche@gmail.com>
2023-01-06 19:37:11 +0100

    main class

---
+++ src/main/java/net/ats/hiempsal/Heimpsal.java
@@ -0,0 +1,24 @@
+//////////////////////////////////////////////////
+//
+//net.ats.hiempsal.Heimpsal.java main Class
+//
+//////////////////////////////////////////////////
+
+package net.ats.hiempsal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author L KHERBICHE
+ * @since 0.0.1-RELEASE
+ */
+@SpringBootApplication
+public class Heimpsal {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Heimpsal.class, args);
+	}
+
+}
