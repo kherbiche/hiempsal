5ab37602bff4d3a570c57763d2b5c7a40bdfe8ac
lyes kherbiche <kerbiche@gmail.com>
2023-01-11 18:59:42 +0100

    EMF EM DS conf

---
+++ src/main/java/net/ats/hiempsal/util/EMUtils.java
@@ -0,0 +1,52 @@
+////////////////////////////////////////////////////////////////////////////////////
+//
+// This following class utilizes an EntityMangerFactory, EntityManger, ThreadLocal
+// to manage persistence and transaction in the application for.
+//
+// The net.ats.hiempsal.util.EMUtils program needs the EntityMangerFactory bean,
+// in order to create EntityManager instance.
+//
+// Method syntax:
+//     EntityManager getEm(String url)
+//
+////////////////////////////////////////////////////////////////////////////////////
+
+package net.ats.hiempsal.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.jpa.repository.support.JpaRepositoryFactory;
+import org.springframework.stereotype.Component;
+
+import net.ats.hiempsal.user.UserRepository;
+
+/**
+ * 
+ * @author L KHERBICHE
+ */
+@Component
+public class EMUtils {
+	@Autowired
+	@Qualifier("h2dbEMF")
+	private EntityManagerFactory emfh2;
+	@Autowired
+	private static final ThreadLocal<EntityManager> threadLocalEnityManagerH2;
+	private EntityManager emh2;
+	static {
+		threadLocalEnityManagerH2 = new ThreadLocal<EntityManager>();
+	}
+	public EntityManager getEM() {
+		emh2 = threadLocalEnityManagerH2.get();
+		if(emh2 == null) {
+			emh2 = emfh2.createEntityManager();
+			threadLocalEnityManagerH2.set(emh2);
+		}
+		return emh2;
+	}
+	public UserRepository getUserRepo(){
+	        return new JpaRepositoryFactory(getEM()).getRepository(UserRepository.class);
+	}
+}
---
+++ src/main/java/net/ats/hiempsal/util/H2DBConfig.java
@@ -0,0 +1,74 @@
+///////////////////////////////////////////////
+//
+// net.ats.hiempsal.util.H2DBConfig.java
+// is conf class
+//
+///////////////////////////////////////////////
+package net.ats.hiempsal.util;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.stream.Collectors;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+*
+* @author L KHERBICHE
+* @since 0.0.1-RELEASE
+*/
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(entityManagerFactoryRef = "h2dbEMF", transactionManagerRef = "h2dbTM", basePackages = "net.ats.hiempsal.user")
+public class H2DBConfig {
+	
+	@Bean("dataSource")
+	@ConfigurationProperties(prefix = "spring.h2db.datasource")
+	public DataSource h2DataSource() {
+		return DataSourceBuilder.create().build();
+	}
+	@Bean
+	public EntityManagerFactoryBuilder entityManagerFactoryBuilder() {
+	   return new EntityManagerFactoryBuilder(new HibernateJpaVendorAdapter(), new HashMap<>(), null);
+	}
+	@SuppressWarnings("unchecked")
+	@Bean(name = "h2dbEMF")
+	public LocalContainerEntityManagerFactoryBean h2dbEntityManagerFactory(EntityManagerFactoryBuilder builder) {  
+		return builder
+				.dataSource(h2DataSource())
+				.properties(hibernateProperties())
+				.packages("net.ats.hiempsal.user")
+				.persistenceUnit("h2dbPU")
+				.build();
+	}
+	@Bean(name = "h2dbTM")
+	public PlatformTransactionManager h2dbTransactionManager(@Qualifier("h2dbEMF") EntityManagerFactory entityMF) {
+		return new JpaTransactionManager(entityMF);
+	}
+	private Map hibernateProperties() {
+		Properties properties = new Properties() {
+			private static final long serialVersionUID = 1L;
+			{
+				setProperty("hibernate.hbm2ddl.auto", "create-drop");
+				setProperty("hibernate.show_sql", "true");
+				setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+			}
+		};
+		return properties.entrySet().stream().collect(Collectors.toMap(e -> e.getKey().toString(), e -> e.getValue()));
+	}
+}
