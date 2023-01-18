ef352e3dcc1b535a92715ecbcc6597b8b9ec50b0
lyes kherbiche <kerbiche@gmail.com>
2023-01-18 18:47:12 +0100

    test controler#post

--- src/test/java/net/ats/hiempsal/user/UControllerTests.java
+++ src/test/java/net/ats/hiempsal/user/UControllerTests.java
@@ -5,17 +5,24 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import net.ats.hiempsal.model.Gender;
 
@@ -30,6 +37,8 @@ public class UControllerTests {
 	private MockMvc mockMvc;
 	@MockBean
 	private UserService service;
+	@Autowired
+    private ObjectMapper mapper;
 	
 	@BeforeEach
 	void setup() {
@@ -47,6 +56,27 @@ public class UControllerTests {
 		ResultActions actions = mockMvc.perform(get("/api/users/3").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isFound());
 		actions.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-				.andExpect(jsonPath("$.id").value(3));
+				.andExpect(jsonPath("$.id").value(3))
+				.andExpect(jsonPath("$.name").value("Micipsa"));
+	}
+	@Test
+	void testProcessCreationSuccess() throws Exception {
+		User u = new User();
+		u.setName("Mastanabal");
+		u.setCountry("France");
+		u.setGender(Gender.Male);
+		u.setPhone("+33666666666");
+		u.setBirthDate(LocalDate.of(103, 10, 01));
+		
+		Mockito.when(service.saveUser(Mockito.any(User.class))).thenReturn(u);
+		
+		MvcResult res = mockMvc.perform(post("/api/users").contentType(MediaType.APPLICATION_JSON)
+	            .content(mapper.writeValueAsString(u).getBytes(StandardCharsets.UTF_8))
+	            .accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isCreated())
+				.andReturn();
+		Assertions.assertThat(res).isNotNull();
+        String userJson = res.getResponse().getContentAsString();
+        Assertions.assertThat(userJson).isEqualToIgnoringCase(mapper.writeValueAsString(u));
 	}
 }
